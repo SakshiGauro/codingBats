b07db68029cb75c9fc03146c46527b9a3d543d03
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-12-07 19:50:28 -0500

    GetSandwich -user interactive

---
+++ string2s/src/main/java/com/string2/GetSandwich.java
@@ -0,0 +1,22 @@
+package com.string2;
+import java.util.Scanner;
+
+public class GetSandwich {
+    public String getSandwich(String str) {
+        int first=str.indexOf("bread");
+        int last=str.lastIndexOf("bread");
+        if((first==-1 || last==-1) || (first==last))
+            return "";
+        else
+            return str.substring(first+5,last);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String word = input.nextLine();
+
+        GetSandwich obj= new GetSandwich();
+        System.out.println(obj.getSandwich(word));
+    }
+}
---
+++ string2s/src/main/java/com/string2/getSandwich.md
@@ -0,0 +1,8 @@
+# GetSandwich
+A sandwich is two pieces of bread with something in between. Return the string that is between the first and last appearance of "bread" in the given string, or return the empty string "" if there are not two pieces of bread.
+```
+getSandwich("breadjambread") → "jam"
+getSandwich("xxbreadjambreadyy") → "jam"
+getSandwich("xxbreadyy") → ""
+```
+https://codingbat.com/prob/p129952
