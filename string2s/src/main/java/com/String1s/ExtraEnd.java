1f1bcc5f7ee71755410c631d289a5beacc4788b8
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-12-12 01:14:46 -0500

    ExtraEnd -user interactive

---
+++ string2s/src/main/java/com/String1s/ExtraEnd.java
@@ -0,0 +1,17 @@
+package com.String1s;
+import java.util.Scanner;
+
+public class ExtraEnd {
+    public String extraEnd(String str) {
+        String rep= str.substring(str.length()-2);
+        return rep+rep+rep;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String word = in.nextLine();
+        ExtraEnd obj = new ExtraEnd();
+        System.out.println(obj.extraEnd(word));
+    }
+}
