1142d457bbb3afc76f2b1444604c0a0bdad197e3
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-12-12 00:53:33 -0500

    HelloName -user interactive

---
+++ string2s/src/main/java/String1s/HelloName.java
@@ -0,0 +1,17 @@
+package String1s;
+import java.util.Scanner;
+
+public class HelloName {
+    public String helloName(String name) {
+        String greet= "Hello "+ name+"!";
+        return greet;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String word = in.nextLine();
+        HelloName obj = new HelloName();
+        System.out.println(obj.helloName(word));
+    }
+}
---
+++ string2s/src/main/java/String1s/helloName.md
@@ -0,0 +1,9 @@
+# HelloName
+
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+```
+helloName("Bob") → "Hello Bob!"
+helloName("Alice") → "Hello Alice!"
+helloName("X") → "Hello X!"
+```
+https://codingbat.com/prob/p171896
