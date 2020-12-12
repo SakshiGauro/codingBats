203c931bc2cee703b9d9fac25200bab6b03580eb
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-12-12 01:18:27 -0500

    firstTwo -user interactive

---
+++ string2s/src/main/java/com/String1s/FirstTwo.java
@@ -0,0 +1,22 @@
+package com.String1s;
+import java.util.Scanner;
+
+public class FirstTwo {
+    public String firstTwo(String str) {
+        if (str.length()>= 2)
+            return str.substring(0,2);
+        else
+        {
+            return str;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String word = in.nextLine();
+        FirstTwo obj = new FirstTwo();
+        System.out.println(obj.firstTwo(word));
+    }
+}
