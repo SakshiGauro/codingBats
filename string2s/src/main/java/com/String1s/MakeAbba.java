ad8f4468d8656b7b07ade6af8ff410f48cec899b
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-12-12 01:01:03 -0500

    MakeAbba -user interactive

---
+++ string2s/src/main/java/com/String1s/MakeAbba.java
@@ -0,0 +1,17 @@
+package com.String1s;
+import java.util.Scanner;
+
+public class MakeAbba {
+    public String makeAbba(String a, String b) {
+        return a+b+b+a;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter 2 words");
+        String word1 = in.nextLine();
+        String word2 = in.nextLine();
+        MakeAbba obj = new MakeAbba();
+        System.out.println(obj.makeAbba(word1, word2));
+    }
+}
---
+++ string2s/src/main/java/com/String1s/makeAbba.md
@@ -0,0 +1,9 @@
+# MakeAbba
+
+Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+```
+makeAbba("Hi", "Bye") → "HiByeByeHi"
+makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+makeAbba("What", "Up") → "WhatUpUpWhat"
+```
+https://codingbat.com/prob/p161056
