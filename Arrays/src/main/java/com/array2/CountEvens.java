c5bd64ed50af344fc48ef4ea5179107b59cf934d
Sakshi Gauro <gaurosakshi113@gmail.com>
2020-12-22 22:22:37 -0500

    countEvens - user interactive

---
+++ Arrays/src/main/java/com/array2/CountEvens.java
@@ -0,0 +1,33 @@
+package com.array2;
+
+import java.util.Scanner;
+
+public class CountEvens {
+    public int countEvens(int[] nums) {
+        if(nums.length ==  0)
+            return 0;
+        else
+        {
+            int c = 0;
+            for (int var : nums)
+            {
+                if (var % 2 == 0)
+                    c++;
+            }
+            return c;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("number of elements in an array ");
+        int[] arr = new int[in.nextInt()];
+        for (int i = 0; i< arr.length; i++){
+            System.out.println("Enter an element");
+            arr[i] = in.nextInt();
+        }
+
+        CountEvens obj = new CountEvens();
+        System.out.println(obj.countEvens(arr));
+    }
+}
---
+++ Arrays/src/main/java/com/array2/countEvens.md
@@ -0,0 +1,8 @@
+# CountEvens
+Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+```
+countEvens([2, 1, 2, 3, 4]) → 3
+countEvens([2, 2, 0]) → 3
+countEvens([1, 3, 5]) → 0
+```
+https://codingbat.com/prob/p162010
