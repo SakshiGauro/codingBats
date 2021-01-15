aa43ee4731607dc0213fb821deedcec1a72fdc40
Sakshi Gauro <gaurosakshi113@gmail.com>
2021-01-15 11:35:42 -0500

    sum67 - user interactive

--- Arrays/src/main/java/com/array2/Sum67.java
+++ Arrays/src/main/java/com/array2/Sum67.java
@@ -1,4 +1,6 @@
 package com.array2;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sum67 {
@@ -7,8 +9,7 @@ public class Sum67 {
             return 0;
         boolean gate = true;
         int sums = 0;
-        for (int i = 0; i < nums.length; i++)
-        {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 6)
                 gate = false;
             if (gate)
@@ -23,10 +24,20 @@ public class Sum67 {
         Scanner in = new Scanner(System.in);
         System.out.println("number of elements in an array ");
         int[] arr = new int[in.nextInt()];
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             System.out.println("Enter an element");
             arr[i] = in.nextInt();
         }
+
+//        Arrays.stream(arr).forEach(el -> {
+//            System.out.println("Enter an element");
+//            arr[el] = in.nextInt();
+//        });
+
+        System.out.println(Arrays.stream(arr).parallel().sum());
+        System.out.println(Arrays.stream(arr).min());
+
+        Arrays.stream(arr).filter(el->el>=5).forEach(System.out::println);
 
         Sum67 obj = new Sum67();
         System.out.println(obj.sum67(arr));
