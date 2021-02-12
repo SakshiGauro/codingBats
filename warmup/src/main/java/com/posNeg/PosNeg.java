822de60969fe756ad72380a6e9aae6f17cd3969e
Sakshi Gauro <gaurosakshi113@gmail.com>
2021-02-11 19:20:18 -0500

    posNeg - user interactive

--- warmup/src/main/java/com/posNeg/PosNeg.java
+++ warmup/src/main/java/com/posNeg/PosNeg.java
@@ -6,8 +6,7 @@ public class PosNeg {
     public boolean posNeg(int a, int b, boolean negative) {
         if(negative)
             return (a < 0 && b < 0);
-        else
-            return ((a < 0 && b > 0) || (a > 0 && b < 0));
+        return ((a < 0 && b > 0) || (a > 0 && b < 0));
     }
 
     public static void main(String[] args) {
