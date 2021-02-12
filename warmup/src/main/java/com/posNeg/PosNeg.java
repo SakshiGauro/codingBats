c91b1384b8e56c20aac0e9c659629dcb8b33565b
Sakshi Gauro <gaurosakshi113@gmail.com>
2021-02-11 19:16:26 -0500

    posNeg - user interactive

---
+++ warmup/src/main/java/com/posNeg/PosNeg.java
@@ -0,0 +1,25 @@
+package com.posNeg;
+
+import java.util.Scanner;
+
+public class PosNeg {
+    public boolean posNeg(int a, int b, boolean negative) {
+        if(negative)
+            return (a < 0 && b < 0);
+        else
+            return ((a < 0 && b > 0) || (a > 0 && b < 0));
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int a = in.nextInt();
+        System.out.println("Enter a number");
+        int b = in.nextInt();
+        System.out.println("Negative is: ");
+        boolean neg = in.nextBoolean();
+
+        PosNeg obj = new PosNeg();
+        System.out.println(obj.posNeg(a,b, neg));
+    }
+}
